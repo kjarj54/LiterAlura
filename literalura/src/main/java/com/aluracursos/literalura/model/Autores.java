ad2883e073435a8ae7937f6ebb8058c422272021
Kevin Arauz <kjarj54@gmail.com>
2024-05-30 22:42:57 +0000

    [UPADTE]

---
+++ literalura/src/main/java/com/aluracursos/literalura/model/Autores.java
@@ -0,0 +1,34 @@
+package com.alura.desafios.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Autores(
+		@JsonAlias("name") String nombre,
+		@JsonAlias("birth_year") short nacimiento,
+		@JsonAlias("death_year") short fallecimiento
+		) {
+
+	public String nombre() {
+		return nombre;
+	}
+
+	public short nacimiento() {
+		return nacimiento;
+	}
+
+	public short fallecimiento() {
+		return fallecimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "\n" + nombre + "\n" +
+			   "nacimiento = " + nacimiento + "\n" +
+			   "fallecimiento = " + fallecimiento;
+	}
+	
+	
+	
+}
